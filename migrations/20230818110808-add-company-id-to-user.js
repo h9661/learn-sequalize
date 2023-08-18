c7aa8d891d2b41eafa80ea4d1709289c6810063a
h9661 <guscksdn123@gmail.com>
2023-08-18 20:28:32 +0900

    롤백 추가

--- migrations/20230818110808-add-company-id-to-user.js
+++ migrations/20230818110808-add-company-id-to-user.js
@@ -21,12 +21,7 @@ module.exports = {
          */
     },
 
-    async down(queryInterface, Sequelize) {
-        /**
-         * Add reverting commands here.
-         *
-         * Example:
-         * await queryInterface.dropTable('users');
-         */
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.removeColumn("Users", "CompanyId");
     },
 };
