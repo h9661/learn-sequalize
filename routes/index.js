23aaba7e01c930d42a929562b9eef1fa9949d2da
h9661 <guscksdn123@gmail.com>
2023-08-21 17:08:29 +0900

    초기 index rendering에 company 추가

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 var express = require("express");
 const { User } = require("../models");
+const Company = require("../models/company");
 var router = express.Router();
 
 /* GET home page. */
 router.get("/", async (req, res, next) => {
     const users = await User.findAll();
-    res.render("sequelize", { users: users });
+    const companies = await Company.findAll();
+    res.render("sequelize", { users: users, companies: companies });
 });
 
 module.exports = router;
